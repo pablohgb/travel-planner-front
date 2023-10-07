6ab54a1ed1c41259339f06bdc18330ffe47569ad
Pablo Herrero <pablohgb@gmail.com>
2023-10-07 19:31:32 +0200

    reworked loginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,16 @@
 import React, { useState } from 'react';
-import { loginUser } from '../../api/auth'
+import { loginUser } from '../../api/auth';
+import Box from '@mui/joy/Box';
+import Stack from '@mui/joy/Stack';
+import Typography from '@mui/joy/Typography';
+import Link from '@mui/joy/Link';
+import Divider from '@mui/joy/Divider';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Checkbox from '@mui/joy/Checkbox';
+import Button from '@mui/joy/Button';
+
 
 function LoginPage() {
   const [username, setUsername] = useState('');
@@ -11,35 +22,115 @@ function LoginPage() {
 
     // TODO: Perform login logic with username and password
     setEmail('');
-    setUsername('');
     setPassword('');
-    const userInfo = { email: email, username: username, password: password };
+    const userInfo = { email: email, password: password };
     loginUser(userInfo)
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <FormLabel
+      sx={(theme) => ({
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        boxShadow: 2
+      })}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '100dvh',
+          width:
+            'clamp(var(--Form-maxWidth), (var(--Collapsed-breakpoint) - 100vw) * 999, 100%)',
+          maxWidth: '100%',
+          px: 2,
+        }}
+      >
+        <Box
+          component="header"
+          sx={{
+            py: 3,
+            display: 'flex',
+            alignItems: 'left',
+            justifyContent: 'space-between',
+          }}
+        ></Box>
+        <Box
+          component="main"
+          sx={{
+            my: 'auto',
+            py: 2,
+            pb: 5,
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            width: 400,
+            maxWidth: '100%',
+            mx: 'auto',
+            borderRadius: 'sm',
+            '& form': {
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 2,
+            },
+            // [`& .${formLabelClasses.asterisk}`]: {
+            //   visibility: 'hidden',
+            // },
+          }}
+        >
+          <Stack gap={4} sx={{ mb: 2 }}>
+            <Stack gap={1}>
+              <Typography level="h3">Sign in</Typography>
+              <Typography level="body-sm">
+                New to company?{' '}
+                <Link href="#replace-with-a-link" level="title-sm">
+                  Sign up!
+                </Link>
+              </Typography>
+            </Stack>
+            <Divider />
+            <Stack gap={4} sx={{ mt: 2 }}>
+              <form
+              // onSubmit={handleSubmit} => {
+              //   event.preventDefault();
+              //   const formElements = event.currentTarget.elements;
+              //   const data = {
+              //     email: formElements.email.value,
+              //     password: formElements.password.value,
+              //     persistent: formElements.persistent.checked,
+              //   };
+              //   alert(JSON.stringify(data, null, 2));
+              // }}
+              >
+                <FormControl required>
+                  <FormLabel>Email</FormLabel>
+                  <Input type="email" name="email" />
+                </FormControl>
+                <FormControl required>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" name="password" />
+                </FormControl>
+                <Stack gap={4} sx={{ mt: 2 }}>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <Checkbox size="sm" label="Remember me" name="persistent" />
+                  </Box>
+                  <Button type="submit" fullWidth>
+                    Sign in
+                  </Button>
+                </Stack>
+              </form>
+            </Stack>
+          </Stack>
+        </Box>
+      </Box>
+    </FormLabel >
   );
 }
 
